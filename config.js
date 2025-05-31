b34c5ce732eb66cbbe1e0ae54b7feb0e1ac900f4
SlimBroken <slimbroken@gmail.com>
2025-06-01 02:04:36 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js - Keep this file LOCAL and private!
 const CONFIG = {
-    GITHUB_TOKEN: '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',
+    GITHUB_TOKEN: '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',
     GIST_ID: '56add5a45986c5face0fcf2f9950627f'
 };
 
