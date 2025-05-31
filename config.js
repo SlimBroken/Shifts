87a76d6085443ec143226aec3fa9a2bccf5e1ef5
SlimBroken <slimbroken@gmail.com>
2025-06-01 01:54:50 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js - Keep this file LOCAL and private!
+const CONFIG = {
+    GITHUB_TOKEN: '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',
+    GIST_ID: '56add5a45986c5face0fcf2f9950627f'
+};
+
+// Make config available globally
+window.SHIFT_SCHEDULER_CONFIG = CONFIG;
