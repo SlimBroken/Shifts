3bffa8c23cc4f7bb0e71d8418fb907f26ec48b9a
SlimBroken <slimbroken@gmail.com>
2025-07-11 05:25:37 +0300

    Update lock.js

--- public/admin/lock.js
+++ public/admin/lock.js
@@ -1,1 +1,10 @@
-export function lockPreferences() { console.log('Lock system initialized'); }
+// lock.js
+export function lockPreferences() {
+  localStorage.setItem('preferencesLocked', 'true');
+  localStorage.setItem('lockTimestamp', Date.now().toString());
+}
+
+export function unlockPreferences() {
+  localStorage.setItem('preferencesLocked', 'false');
+  localStorage.removeItem('lockTimestamp');
+}
